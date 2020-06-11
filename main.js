bcb18f0ad864160266c3f21acfd5fb5bcc3b3c59
zhanglei260 <121484145@qq.com>
2020-06-11 14:46:59 +0800

    宁APP init

---
+++ main.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import App from './App'
+
+import basics from './pages/basics/home.vue'
+Vue.component('basics',basics)
+
+import components from './pages/component/home.vue'
+Vue.component('components',components)
+
+import plugin from './pages/plugin/home.vue'
+Vue.component('plugin',plugin)
+
+import cases from './pages/basics/home.vue'
+Vue.component('case',cases)
+import books from './pages/component/home.vue'
+Vue.component('book',books)
+import addcases from './pages/plugin/home.vue'
+Vue.component('addcase',addcases)
+import crms from './pages/basics/home.vue'
+Vue.component('crm',crms)
+import mys from './pages/component/home.vue'
+Vue.component('my',mys)
+
+import cuCustom from './colorui/components/cu-custom.vue'
+Vue.component('cu-custom',cuCustom)
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+
+ 
+
+
+
